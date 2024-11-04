15e8845421fb5d462af9f8078fdb9dd9568aa1e6
DESKTOP-9C48CIB\datho <dat>
2024-11-04 21:48:05 +0700

    Up

---
+++ Module_4/AppTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using DAL;
+
+var repository = new ProductRepository();
+var list = await repository.GetProducts();
+if (list != null)
+{
+    foreach (var item in list)
+    {
+        Console.WriteLine(item.ProductId);
+        Console.WriteLine(item.ProductName);
+    }
+}
---
+++ Module_4/WebAppClassLib/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace WebAppClassLib.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
