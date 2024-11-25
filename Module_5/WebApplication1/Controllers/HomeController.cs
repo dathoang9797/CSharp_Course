61c8fa550b322f2fa901d8da2daae9ec260a84ae
DESKTOP-9C48CIB\datho <dat>
2024-11-25 23:35:39 +0700

    Up

--- Module_5/WebApplication1/Controllers/HomeController.cs
+++ Module_5/WebApplication1/Controllers/HomeController.cs
@@ -8,6 +8,13 @@ public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
 
+    private readonly List<Department> _list =
+    [
+        new Department { Id = 1, Name = "A", Description = "Description A" },
+        new Department { Id = 2, Name = "B", Description = "Description B" },
+        new Department { Id = 3, Name = "C", Description = "Description C" }
+    ];
+
     public HomeController(ILogger<HomeController> logger)
     {
         _logger = logger;
@@ -17,6 +24,40 @@ public class HomeController : Controller
     {
         //ViewData
         //ViewBag
-        return View();
+        ViewData["Title"] = "Web App Store";
+        ViewData["Description"] = "Description for web app Store";
+        ViewData["a"] = 7;
+        ViewData["b"] = 9;
+
+
+        ViewBag.Departments = _list;
+        ViewBag.Total = _list.Count;
+        var departments = new DepartmentTotal
+        {
+            Departments = _list,
+            Total = _list.Count
+        };
+        return View(departments);
+    }
+
+    public IActionResult DeleteAll(int[] ids)
+    {
+        return View(ids);
+    }
+    
+    [HttpPost]
+    public IActionResult AddAll(List<Department> list)
+    {
+        return View(list);
+    }
+
+    public IActionResult Dynamic()
+    {
+        return View(new { Departments = _list, Total = _list.Count });
+    }
+
+    public IActionResult JsonData()
+    {
+        return Json(new { Departments = _list, Total = _list.Count });
     }
 }
---
+++ Module_5/WebApplication1/Models/DepartmentTotal.cs
@@ -0,0 +1,7 @@
+namespace WebApplication1.Models;
+
+public class DepartmentTotal
+{
+    public List<Department> Departments { get; set; }
+    public int Total { get; set; }
+}
