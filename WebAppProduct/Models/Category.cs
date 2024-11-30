b8a39fb4c76004688ac33f7a790011efb9c5e80b
DESKTOP-9C48CIB\datho <dat>
2024-11-30 17:24:05 +0700

    Up

--- WebAppProduct/Models/Category.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace WebAppProduct.Models;
-
-public class Category
-{
-    public int CategoryId { get; set; }
-    [Required] [MaxLength(100)] public string? CategoryName { get; set; }
-}
