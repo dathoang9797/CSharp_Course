d90c9fadd3ecd714fd4d0e48118b6b29475f2526
DESKTOP-9C48CIB\datho <dat>
2024-12-16 20:42:25 +0700

    Up

---
+++ WebAppFruitable/Areas/Dashboard/Controller/RoleController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using WebAppFruitable.Controllers;
+using WebAppFruitable.Model;
+
+namespace WebAppFruitable.Areas.Dashboard.Controller;
+
+[Area("dashboard")]
+public class RoleController : BaseController
+{
+    [Authorize]
+    public IActionResult Index()
+    {
+        var listRole = Provider.Role.GetRoles();
+        return View(listRole);
+    }
+
+    [Authorize]
+    [HttpPost]
+    public IActionResult Add(Role obj)
+    {
+        var ret = Provider.Role.Add(obj);
+        return ret > -1 ? Redirect("/dashboard/role") : Redirect("/dashboard/err");
+    }
+
+    [Authorize]
+    [HttpPost]
+    [Route("/dashboard/role/delete/{id}")]
+    public IActionResult Delete([FromRoute] int id)
+    {
+        //id 1 is Admin
+        if (id == 1)
+        {
+            TempData["Msg"] = "Can't delete admin";
+            return Redirect("/dashboard/role");
+        }
+
+        var ret = Provider.Role.Delete(id);
+        return ret > -1 ? Redirect("/dashboard/role") : Redirect("/dashboard/err");
+    }
+}
--- WebAppFruitable/Repository/RoleRepository.cs
+++ WebAppFruitable/Repository/RoleRepository.cs
@@ -11,9 +11,37 @@ public class RoleRepository : BaseRepository
     public RoleRepository(FruitableContext context) : base(context)
     {
     }
+    
+    public List<Role> GetRoles()
+    {
+        return Context.Role.ToList();
+    }
 
     public List<RoleChecked> GetRolesByMember(string memberId)
     {
         return Context.Database.SqlQuery<RoleChecked>($"EXEC GetRolesByMember @Id ={memberId}").ToList();
+    }
+    
+    public int Add(Role? role)
+    {
+        if (role != null)
+        {
+            Context.Role.Add(role);
+            return Context.SaveChanges();
+        }
+
+        return -1;
+    }
+    
+    public int Delete(int id)
+    {
+        var role = Context.Role.Find(id);
+        if (role != null)
+        {
+            Context.Role.Remove(role);
+            return Context.SaveChanges();
+        }
+
+        return -1;
     }
 }
