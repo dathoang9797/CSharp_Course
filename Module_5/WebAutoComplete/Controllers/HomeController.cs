f97b3ddf4c98f42b318793bd69d89d359eae9bad
DESKTOP-9C48CIB\datho <dat>
2024-12-22 13:49:11 +0700

    Up

--- Module_5/WebAutoComplete/Controllers/HomeController.cs
+++ Module_5/WebAutoComplete/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ public class HomeController : Controller
     {
         return View();
     }
+    
+    public IActionResult Multiple()
+    {
+        return View();
+    }
 
     public IActionResult LoadData(string q)
     {
